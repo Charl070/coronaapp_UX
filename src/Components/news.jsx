534261dac5e6846d3443c020234ccfd1aec811bd
Charl070 <charlesjnr070@gmail.com>
2020-03-25 17:34:55 +0200

    news page

---
+++ src/Components/news.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import './news.css';
+import Dambudzo from '../assets/pres.jpg'
+
+function News() {
+  return (
+   <div>
+       <ul>
+  <li>
+      <div className="news-container-flex">
+        <div className="news-image">
+            image
+        </div>
+        <div className="title-position text-left">
+        New Testing Kits In Harare
+        </div>
+      </div>
+     
+  </li>
+  <li>
+      <div className="news-container-flex">
+        <div className="news-image">
+            image
+        </div>
+        <div className="title-position text-left">
+        New Testing Kits In Harare
+        </div>
+      </div>
+     
+  </li>
+  <li>
+      <div className="news-container-flex">
+        <div className="news-image">
+            image
+        </div>
+        <div className="title-position text-left">
+        New Testing Kits In Harare
+        </div>
+      </div>
+     
+  </li>
+  <li>
+      <div className="news-container-flex">
+        <div className="news-image">
+            image
+        </div>
+        <div className="title-position text-left">
+        New Testing Kits In Harare
+        </div>
+      </div>
+     
+  </li>
+  <li>
+      <div className="news-container-flex">
+        <div className="news-image">
+            image
+        </div>
+        <div className="title-position text-left">
+        New Testing Kits In Harare
+        </div>
+      </div>
+     
+  </li>
+  <li>
+      <div className="news-container-flex">
+        <div className="news-image">
+            image
+        </div>
+        <div className="title-position text-left">
+        New Testing Kits In Harare
+        </div>
+      </div>
+     
+  </li>
+</ul>
+   </div>
+  );
+}
+
+export default News;
