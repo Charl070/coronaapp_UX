bfef6239f939f7649e56f07f46a7b59f86e92ff7
Charl070 <charlesjnr070@gmail.com>
2020-03-26 19:09:23 +0200

    new develop

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import PictureMap from './components/PictureMap';
-import Footer from './components/Footer';
-import Navigation from './components/Navigation';
+import News from './Components/news'
+import WorldNews from './Components/worldnews'
+import Twitter from './Components/twitter';
+import Safety from './Components/Safety';
 
-require('typeface-montserrat');
-require('typeface-roboto');
 
 function App() {
   return (
-    <BrowserRouter>
     <div className="App">
-      <Navigation/>
-      <PictureMap/>
-      <Footer/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <News/>
+      <WorldNews/>
+      <Safety/>
+      <Twitter/>
     </div>
-    </BrowserRouter>
   );
 }
 
