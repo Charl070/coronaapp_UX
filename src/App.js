dc9f8aafdf9666c69cc7c754deeac56427045af9
Anesu <murac96@gmail.com>
2020-03-26 18:15:59 +0200

    front page done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import PictureMap from './components/PictureMap';
+import Footer from './components/Footer';
+import Navigation from './components/Navigation';
+
+require('typeface-montserrat');
+require('typeface-roboto');
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-        </a>
-      </header>
+      <Navigation/>
+      <PictureMap/>
+      <Footer/>
     </div>
+    </BrowserRouter>
   );
 }
 
