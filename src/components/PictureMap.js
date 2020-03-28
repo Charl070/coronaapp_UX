f8ed238ebbec7a7fa9c34b5abb0be1820dac879b
Anesu <murac96@gmail.com>
2020-03-28 20:47:39 +0200

    provincial disclaimer

--- src/components/PictureMap.js
+++ src/components/PictureMap.js
@@ -88,6 +88,7 @@ export default class PictureMap extends React.Component {
                   
 
                     <p className="body">Last Updated {this.state.date} </p>
+                    <p className="body">Some Cases are of Unknown Provincial Distritution</p>
 
                 </div>
 
